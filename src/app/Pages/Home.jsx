9d76ea1d1140a9775f358c201f0b0fb805e0882c
Danny Bermudez <danny@Dannys-MacBook-Pro.local>
2023-02-18 13:24:39 -0800

    button color fix

--- src/app/Pages/Home.jsx
+++ src/app/Pages/Home.jsx
@@ -37,7 +37,7 @@ const Home = () => {
 							<button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>
 								Play
 							</button>
-							<button className='border text-white border-gray-300 py-2 px-5 ml-4'>
+							<button className='border text-black bg-gray-300 border-gray-300 py-2 px-5 ml-4'>
 								Watch later
 							</button>
 						</div>
